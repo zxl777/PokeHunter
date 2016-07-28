13240e6418eafe46dd12753e755cbb1e72d510e0
sky <sky@itoytoy.com>
2016-07-29 07:46:01 +0800

    显示图钉〰〰〰📥2016-07-29🕑07:46:01

--- PokeHunterApps/Poke Hunter/Map.m
+++ PokeHunterApps/Poke Hunter/Map.m
@@ -32,6 +32,11 @@
     MKCoordinateRegion region = MKCoordinateRegionMake(center, span);
     
     [self.Map setRegion:region animated:YES];
+
+    BasicMapAnnotation *annotation=[[BasicMapAnnotation alloc] initWithLatitude:34.0522342 andLongitude:-118.2436849 tag:0];
+    
+    annotation.title = @"Hello";
+    [self.Map addAnnotation:annotation];
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,5 +45,58 @@
 }
 
 
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    // create a proper annotation view, be lazy and don't use the reuse identifier
+    MKAnnotationView *view = [[MKAnnotationView alloc] initWithAnnotation:annotation
+                                                          reuseIdentifier:@"identifier"];
+    
+    // create a disclosure button for map kit
+    UIButton *disclosure = [UIButton buttonWithType:UIButtonTypeContactAdd];
+    
+    [disclosure addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                             action:@selector(disclosureTapped)]];
+    view.rightCalloutAccessoryView = disclosure;
+    
+    view.enabled = YES;
+    view.image = [UIImage imageNamed:@"scan"];
+    
+    view.canShowCallout = YES;
+    
+    return view;
+}
+
+
+//- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+//
+//{
+//    static NSString *pinID = @"pinID";
+//    MKAnnotationView *customPinView = [mapView dequeueReusableAnnotationViewWithIdentifier:pinID];
+//    if (!customPinView) {
+//        customPinView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:pinID];
+//    }
+//    
+//    // 设置大头针图片
+//    customPinView.image = [UIImage imageNamed:@"scan"];
+//    
+//    // 设置大头针可以弹出标注
+//    customPinView.canShowCallout = YES;
+//    
+//    // 设置标注左侧视图
+//    UIImageView *leftIV = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+//    leftIV.image = [UIImage imageNamed:@"scan"];
+//    customPinView.leftCalloutAccessoryView = leftIV;
+//    
+//    // 设置标注右侧视图
+//    UIImageView *rightIV = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+//    rightIV.image = [UIImage imageNamed:@"scan"];
+//    customPinView.rightCalloutAccessoryView = rightIV;
+//    
+//    // 设置标注详情视图（iOS9.0）
+//    customPinView.detailCalloutAccessoryView = [[UISwitch alloc] init];
+//    
+//    return customPinView;
+//}
 
 @end
