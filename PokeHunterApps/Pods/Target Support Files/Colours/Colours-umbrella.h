4f8ba8d4d5516179da08af85094f590282f05228
sky <sky@itoytoy.com>
2016-08-02 00:02:25 +0800

    看VLC代码〰〰〰📥2016-08-02🕑00:02:25

--- PokeHunterApps/Pods/Target Support Files/Colours/Colours-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "Colours.h"
-
-FOUNDATION_EXPORT double ColoursVersionNumber;
-FOUNDATION_EXPORT const unsigned char ColoursVersionString[];
-
--- PokeHunterApps/Pods/Target Support Files/Pods-Poke Hunter/Pods-Poke Hunter-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Poke_HunterVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Poke_HunterVersionString[];
-
